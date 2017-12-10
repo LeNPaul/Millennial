cf9287c6ce653633eca9d4e0b06e72c54d6ddb7e
Paul Le <l.nguyen.paul@gmail.com>
2017-12-10 18:16:40 -0500

    Update hover effect for mobile devices

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,6 +1,3 @@
 $(document).ready(function() {
-  var touchsupport = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0)
-  if (!touchsupport){ // browser doesn't support touch
-      document.documentElement.className += " non-touch"
-  }
+  
 });

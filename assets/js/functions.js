ff7909bb8c1899131860d4174f728554c1c0e851
Paul Le <l.nguyen.paul@gmail.com>
2017-12-10 17:52:16 -0500

    Update hover effect for mobile devices

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
-  $('body').bind('touchstart', function() {});
+  var touchsupport = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0)
+  if (!touchsupport){ // browser doesn't support touch
+      document.documentElement.className += " non-touch"
+  }
 });
